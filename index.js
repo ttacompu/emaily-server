18348e8b948debb0d9556318c699bf6146e08156
Thein Aung <ttacompu@gmail.com>
2020-04-10 21:31:52 -0400

    oauth

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 const app = express();
 
-app.get("/", (req, res) => res.send({ test : "hello!"}));
+
+
+passport.use(new GoogleStrategy({
+  clientID : keys.googleClientID,
+  clientSecret : keys.googleClientSecret,
+  callbackURL : "/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) =>{
+   console.log('access token', accessToken);
+   console.log('refresh token', refreshToken);
+   console.log('detail profile', profile)
+  
+}));
+
+app.get("/auth/google", passport.authenticate('google', {
+  scope : ['profile', 'email']
+}));
+
+app.get("/auth/google/callback", passport.authenticate('google'))
+
 
 app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
