3723486e801b47d0394b2a58a83ac08dff84d556
Thein Aung <ttacompu@gmail.com>
2020-06-17 18:25:33 -0400

    heroku deployment

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.use(passport.session());
 routes(app);
 billingRoutes(app);
 
+if(process.env.NODE_ENV === 'production'){
+  //Express 
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get("*", (req, res) =>{
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+}
+
 
 app.listen(process.env.PORT || 5000, function () {
   console.log(
